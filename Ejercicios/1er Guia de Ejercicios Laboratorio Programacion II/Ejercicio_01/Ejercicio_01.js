40e35b7fc6fa4dac2475eec40374624df1dd1c48
Steven <steven@Air-de-Steven.fibertel.com.ar>
2025-08-31 15:10:51 -0300

    Convertimos archivos a typeScript

--- Ejercicios/1er Guia de Ejercicios Laboratorio Programacion II/Ejercicio_01/Ejercicio_01.js
+++
@@ -1,6 +0,0 @@
-// Primer Guia de Ejercicios Laboratorio - Programacion II
-
-// EJERCICIO 01
-/* Crear una clase Persona que tenga los atributos públicos nombre y apellido.
-● Crear una instancia y asignarle valores.
-● Mostrar por pantalla los valores asignados. */
